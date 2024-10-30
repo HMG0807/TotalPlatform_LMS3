d8aac02b455c788bd098fd1ab3a797219b6ef01a
kangila <ia326466@gmail.com>
2024-10-30 13:00:50 +0900

    2024-10-30 [2차 병합 요청]

---
+++ pinguClass/src/main/java/com/example/demo/lms/payment/SubPaymentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.lms.payment;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class SubPaymentService {
+
+	private final SubPaymentRepository spr;
+	
+}
---
+++ pinguClass/src/main/java/com/example/demo/lms/payment/subscribeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.lms.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.lms.entity.Subscribe;
+
+public interface subscribeRepository extends JpaRepository<Subscribe, Integer>{
+
+}
