74c5cf029a3c530acf7cfb81e8495b3b95156784
HMG0807 <besil1317@naver.com>
2024-11-01 16:28:27 +0900

    Revert "1101 이순 회원가입/로그인(오류수정중)"

--- pinguClass/src/main/java/com/example/demo/lms/user/UserService.java
+++ pinguClass/src/main/java/com/example/demo/lms/user/UserService.java
@@ -1,9 +1,7 @@
 package com.example.demo.lms.user;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.lms.entity.User;
@@ -15,9 +13,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class UserService {
 	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
 	
-	public User getUser(Integer id) throws Exception {
+	public User getUser(String id) throws Exception {
 		Optional<User> user = this.userRepository.findById(id);
 		if(user.isPresent()) {
 			return user.get();
@@ -26,28 +23,4 @@ public class UserService {
 			throw new Exception("해당 유저가 존재하지 않습니다");
 		}
 	}
-	
-
-
-	public User returncreate(String id, String email1, String email2, String pw, String name, String tel, String gender,
-			String birth) {
-		
-		User setUser = new User();
-		setUser.setId(id);
-		setUser.setEmail(email1+'@'+email2);
-		setUser.setPw(passwordEncoder.encode(pw));
-		setUser.setName(name);
-		setUser.setTel(tel);
-		setUser.setBirth(birth.replace("-", ""));
-		setUser.setGender(gender);
-		setUser.setBannedYn("n");
-		setUser.setSignoutYn("n");
-		setUser.setCreateDate(LocalDateTime.now());
-		
-		this.userRepository.save(setUser);
-		
-		return setUser;
-	}
-
-
 }
